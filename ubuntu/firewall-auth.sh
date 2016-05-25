b686c15c10af6c3081accf9c0110e662500ff13c
Hrishikesh <hrishirt@cse.iitk.ac.in>
2016-05-25 10:46:40 +0530

    upstart based startup, with install script, for ubuntu

---
+++ ubuntu/firewall-auth.sh
@@ -0,0 +1,127 @@
+#!/bin/sh
+
+CONFIG="$HOME/.iitk-config"
+[ -f $CONFIG ] || CONFIG="/usr/share/iitk-auth/config"
+[ -f $CONFIG ] || (logger -sit Fortigate "No config file found." && exit 1)
+
+username="`sed -n '1 p' ${CONFIG}`"
+password="`sed -n '2 p' ${CONFIG}`"
+
+
+google="http://216.58.220.3"
+curl_opts="-k -m3 -s --stderr /dev/null"
+
+
+trap logout 1 2 3 15
+
+log() {
+    export ts="`date +[%b\ %e\ %H:%M:%S]`"
+    echo $ts $@
+    logger -t Fortigate $@
+ }
+ 
+login() {
+    fgt_redirect=$(
+        curl ${curl_opts} --max-redirs 0 -D- ${google}
+    )
+    if [ -z "${fgt_redirect}" ];
+    then
+        state="fail"
+    elif [ -z "$(echo ${fgt_redirect} | grep "HTTP\/1.1 303 See Other")" ];
+    then
+        state="login"
+    else
+        fgt_auth_url=$(
+            echo "${fgt_redirect}" |
+            sed -n -e 's/.*Location: \(.*\).*/\1/p' |
+            tr -d '\r\n'
+        )
+        fgt_auth_resp=$(
+            curl ${curl_opts} ${fgt_auth_url}
+        )
+        fgt_auth_magic=$(
+            echo "${fgt_auth_resp}" |
+            sed -n -e 's/.*name="magic" \+value="\([^"]\+\).*/\1/p'
+        )
+        fgt_post_resp=$(
+            curl ${curl_opts} -d \
+                "username=${username}&password=${password}&magic=${fgt_auth_magic}&4Tredir=/" \
+                "${fgt_auth_url}"
+        )
+        fgt_keepalive_url=$(
+            echo "${fgt_post_resp}" |
+            sed -n -e 's/.*location.href="\([^"]\+\).*/\1/p' |
+            tr -d '\r\n'
+        )
+        if [ -z "${fgt_keepalive_url}" ];
+        then
+            state="badauth"
+        else
+            log "Logged in"
+            fgt_logout_url=$(
+                echo "${fgt_post_resp}" |
+                sed -n -e 's/.*<p><a href="\([^"]\+\).*/\1/p' |
+                tr -d '\r\n'
+            )
+            state="keepalive"
+        fi
+    fi
+}
+
+keepalive() {
+    fgt_keepalive_resp=$(
+        curl ${curl_opts} -D- ${fgt_keepalive_url}
+    )
+    if [ -z "$(echo "${fgt_keepalive_resp}" | grep "HTTP\/1.1 200 OK")" ];
+    then
+        state="retry"
+    else
+        state="keepalive"
+    fi
+}
+
+logout() {
+    if [ -n "${fgt_logout_url}" ];
+    then
+        log "Logging out"
+        curl ${curl_opts} ${fgt_logout_url} >/dev/null
+    fi
+    exit
+}
+
+login
+while :
+do
+    case ${state} in
+        "fail")
+            log "Network failure"
+            sleep 60 & wait $!
+            login
+            ;;
+        "login")
+            log "Already logged in"
+            sleep 60 & wait $!
+            login
+            ;;
+        "badauth")
+            log "Bad credentials"
+            sleep 240 & wait $!
+            login
+            ;;
+        "retry")
+            log "Retrying login"
+            sleep 1 & wait $!
+            login
+            ;;
+        "keepalive")
+            log "Keeping alive"
+            sleep 300 & wait $!
+            keepalive
+            ;;
+        *)
+            log "Something went wrong"
+            sleep 30 & wait $!
+            login
+            ;;
+    esac
+done
---
+++ ubuntu/install.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+show_help() {
+    cat<<EOF
+Syntax: `basename $0` [option]
+
+Options:
+    -i      install
+    -r      remove
+    -l      re-enter config details
+    -h      help
+
+Report bugs to <hrishirt@iitk.ac.in>
+EOF
+}
+
+check_access() {
+    echo "Installation requires root privileges."
+    sudo touch $HOME/.sudo_success
+
+    if [ ! -f $HOME/.sudo_success ]; then
+        echo "Aborted." > /dev/stderr
+        exit 1;
+    fi
+}
+
+get_info() {
+    echo -n "Enter IITK username: "
+    read user
+    echo -n "Enter IITK password: "
+    read -s pass
+    export ip="`curl -s http://home.iitk.ac.in/~hrishirt/ip/?clean`"
+    echo "Machine IP: $ip"
+    echo -n "Is this IP correct? (y/n): "
+    read -n1 answer
+    while [ "${answer,,}" != "y" ] && [ "${answer,,}" != "n" ]; do
+        read -n1 answer
+        echo -en '\b'
+    done
+                                    
+    echo
+    if [ "${answer,,}" == "n" ]; then
+        echo -n "Enter Machine IP: "
+        read ip
+    fi
+    echo -en "${user}\n${pass}\n${ip}" > $HOME/.iitk-config
+}
+
+copy_user_files() {
+    [ -d $HOME/bin ] || mkdir $HOME/bin
+
+    chmod -v 755 *-auth.sh
+    cp *-auth.sh *.conf $HOME/bin
+    chmod -v 644 *
+
+#    sed -i "s:IITK_USERNAME:$user:" $HOME/bin/firewall-auth.sh $HOME/bin/ironport-auth.sh
+#    sed -i "s:IITK_PASSWORD:$pass:" $HOME/bin/firewall-auth.sh $HOME/bin/ironport-auth.sh
+#    sed -i "s:MACHINE_IP:$ip:" $HOME/bin/ironport-auth.sh
+}
+
+install() {
+    sudo cp -v $HOME/bin/firewall-auth.sh /usr/sbin/iitk-fortigate
+    sudo cp -v $HOME/bin/ironport-auth.sh /usr/sbin/iitk-ironport
+
+    sudo cp -v *conf /etc/init/
+
+    sudo mkdir -pv /usr/share/iitk-auth/
+    sudo cp -v $HOME/.iitk-config /usr/share/iitk-auth/config
+    sudo chmod 600 /usr/share/iitk-auth/config
+}
+
+uninstall() {
+    sudo rm -v /usr/sbin/iitk-fortigate /usr/sbin/iitk-ironport /etc/init/fortigate.conf /etc/init/ironport.conf
+    sudo rm -v $HOME/bin/firewall-auth.sh $HOME/bin/ironport.sh
+    sudo rm -v $HOME/.iitk-config /usr/share/iitk-auth/config
+}
+
+restart_system() {
+    action="uninstall"
+    [ $1 ] || action="install"
+    echo "System needs a restart for $action to complete."
+    echo -n "Restart now? (y/n): "
+    read -n1 answer
+    while [ "${answer,,}" != "y" ] && [ "${answer,,}" != "n" ]; do
+        read -n1 answer
+        echo -en '\b'
+    done
+
+    echo
+    if [ "${answer,,}" == "y" ]; then
+        sudo reboot
+    fi
+}
+
+case $1 in
+    "-l")
+        get_info
+        ;;
+    "-i")
+        check_access
+        get_info
+        copy_user_files
+        install
+        restart_system
+        ;;
+    "-r")
+        check_access
+        uninstall
+        restart_system 1
+        ;;
+    "-h")
+        show_help
+        exit 0
+        ;;
+    *)
+        show_help
+        exit 1
+        ;;
+esac
+
+
+exit 0
